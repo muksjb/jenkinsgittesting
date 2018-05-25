40712e5c6f180aff162b628be1e1fce870ae047a
Mukesh <Mukesh@Mukesh-PC>
2018-05-25 15:27:41 +0530

    first commit

---
+++ src/test/java/GitJenkinpakage/NewTest.java
@@ -0,0 +1,14 @@
+package GitJenkinpakage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get("https://mvnrepository.com/artifact/org.apache.maven.plugins/maven-surefire-plugin/2.21.0");
+  }
+}
