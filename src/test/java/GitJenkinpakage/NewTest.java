dc404711f69227ab5fcadccbfdda233550498fba
Mukesh <Mukesh@Mukesh-PC>
2018-05-26 14:33:40 +0530

    chrome

--- src/test/java/GitJenkinpakage/NewTest.java
+++ src/test/java/GitJenkinpakage/NewTest.java
@@ -2,13 +2,14 @@ package GitJenkinpakage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
-	  
-	  WebDriver driver = new FirefoxDriver();
+	  System. setProperty("webdriver.chrome.driver", "C:\\Jenkins\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
 	  driver.get("https://mvnrepository.com/artifact/org.apache.maven.plugins/maven-surefire-plugin/2.21.0");
 	  driver.close();
   }
